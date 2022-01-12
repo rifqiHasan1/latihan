45f1cb69fb07ef9c6191a9ee0b5cd1630b01f98a
Rifqi Hasan <hasanrifqi0905@gmail.com>
2022-01-12 13:55:45 +0700

    tambah package

--- config/config.js
+++ config/config.js
@@ -21,7 +21,13 @@ module.exports = {
       password: process.env.DB_PASSWORD,
       database: process.env.DB_DATABASE,
       host: process.env.DB_HOST,
-      dialect: process.env.DB_DIALECT
+      dialect: process.env.DB_DIALECT,
+      dialectOptions: {
+        ssl: {
+          require : true,
+          rejectUnauthorized: false,
+        }
+      }
     },
 };
 
